fdc042d794631457376e5121aca8e9264656a92b
sagarKulthe005 <kulthesagar005@yahoo.com>
2016-09-29 19:57:15 +0530

    bump version

--- gulpFile.js
+++ gulpFile.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/**version: 0.0.2 */
 //Gulp in-built packages 
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
@@ -8,6 +8,24 @@ var jslint = require('gulp-jslint');
 var jscs = require('gulp-jscs');
 var guppy = require('git-guppy')(gulp);
 var jsdoc = require('gulp-jsdoc3');
+var bump = require('gulp-bump');
+var git  = require('gulp-git');
+var push = require('gulp-git-push');
+
+gulp.task('bump', function() {
+  return gulp.src(['./gulpFile.js'])
+        // bump package.json version 
+        .pipe(bump({type: 'patch'}))
+        // save bumped file into filesystem 
+        .pipe(gulp.dest('./'))
+        // commit changes 
+        .pipe(git.commit('bump version'))
+        // push local changes to repository 
+        .pipe(push({                      
+            repository: 'origin',
+            refspec: 'HEAD:development'
+        }));
+});
 
 gulp.task('doc', function (cb) {
     gulp.src(['./api/**/*.js'], {read: false})
